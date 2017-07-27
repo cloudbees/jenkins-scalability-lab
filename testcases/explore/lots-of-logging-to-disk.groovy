df081565b135a34d5fc4c1f1970586652ba1a2e1
kshultzCB <kshultz@cloudbees.com>
2017-07-27 11:40:42 -0400

    Check in before changing all the things

--- testcases/explore/lots-of-logging-to-disk.groovy
+++ testcases/explore/lots-of-logging-to-disk.groovy
@@ -10,13 +10,16 @@ for (int i = 0; i < 5; i++) {
 
     stage ("Stage $i") {
         for (int j = 0; j < 5; j++) {
-	        // TODO: 
-            // Work on a shell step that generates its own 
-            // amount of test data for logging, so that it's 
-            // always a fixed amount of logging. More consistent
-            // that way.
             node {
-                sh 'for i in `seq 1 100`; do echo "running loop $i"; netstat -a; done'
+                // Basic ls because there's no netstat on the lightweight system we're on
+                //   sh 'for i in `seq 1 100`; do echo "running loop $i"; netstat -a; done'
+                // This one is trying to restrict to alphanumerics. It also doesn't work.
+                //   sh 'for i in `seq 1 100`; do cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 3200 | head -n 1; done'
+                // Without the restriction to alphanumeric. This one works but generates gibberish.
+                // Might be fine.
+                //   sh 'for i in `seq 1 100` do cat /dev/urandom fold -w 500 | head -n 1; done'
+                // Sams thing
+                sh 'for i in `seq 1 100`; do echo "Stage $i Loop $j"; cat /dev/urandom | env LC_CTYPE=c tr -dc \'[:alpha:]\' | fold -w 320 | head -n 1; done'
             }
         }
     }
