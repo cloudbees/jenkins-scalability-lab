2e8d5a23a78efda343ed4c95250bcd96b2c894cb
kshultzCB <kshultz@cloudbees.com>
2017-07-27 17:20:36 -0400

    I want to track the snippets folder after all. Also, merge sort.

--- testcases/explore/steps-and-logging.groovy
+++ testcases/explore/steps-and-logging.groovy
@@ -5,7 +5,7 @@ Steps, with logging.
 */
 
 for (int i=0; i<10; i++) {
-	stage ("Stage in Branch $i") {
+	stage ("Stage $i") {
 		// Some echos.
 		for (int j=0; j < 20; j++) {
 			// Put in some logging.
--- testcases/snippets/merge-sort.groovy
+++ testcases/snippets/merge-sort.groovy
@@ -1,3 +1,50 @@
-/*
-Oh FFS.
-*/
+
+
+int[] firstThing = [1, 2, 3]
+int[] secondThing = [4, 5, 6]
+
+
+System.out.println(firstThing);
+System.out.println(secondThing);
+
+merge (firstThing, secondThing);
+
+int[] merge(int[] first, int[] second) {
+    System.out.println("--> first is:  " + first);
+    System.out.println("--> second is: " + second);
+	int[] output = new int[first.length + second.length];
+    System.out.println("--> output[] should be uninit'd: " + output + " with length of " + output.length);
+	int idx = 0;
+	int i=0;
+	int j=0;
+
+	// Merge lists
+	while (i < first.length && j < second.length) {
+        System.out.println("--> Are we even getting into this while loop?");
+		int v1 = first[i];
+		int v2 = second[j];
+		if (v1 > v2) {
+			output[idx++] = v1; 
+			i++;
+            System.out.println("--> output is now: " + output );	
+		} else {
+			output[idx++] = v2;
+			j++;
+		}
+	}
+
+	if ( i < first.length) {
+		for (int temp = i; i < first.length; i++) {
+			output[idx++] = first[i];
+            // System.out.println("   --> output is currently " + output);
+		}
+	} else if (j < second.length) {
+		for (int temp = j; j < second.length; j++) {
+			output[idx++] = second[j];
+		}
+	}
+		throw new IllegalStateException("Never happens?");
+	// }
+
+	return output;
+}
