62008ad1c4c2ca0e44a1b939a9a7703249c3f303
kshultzCB <kshultz@cloudbees.com>
2017-07-27 13:23:41 -0400

    More things to check in before I ruin something.

--- testcases/assess/sams-nightmare.groovy
+++ testcases/assess/sams-nightmare.groovy
@@ -1,3 +1,33 @@
 /*
-lol
+sams-nightmare.groovy
+
+This is the "enterprise-y" pipeline. Which is to say, when 
+people do exotic , complex, or unexpected things from within 
+a pipeline, it's often large enterprises that do so. They 
+necessarily have complex processes for build, archive, test,
+deploy, and report steps.
+
+Here's what we want:
+
+- Both CPS and non-CPS Groovy, to invoke steps and do processing, dynamically,
+- Reading and writing lg. files as inputs to steps
+- Running mix of shell (fast), shell (long) steps
+- Lots of echos, maybe half the total number of steps in the pipeline
+- >=1 parallel, doing "some stuff"
+- Stashing/unstashing large files
+- Archive artifacts
+- XML parsing?
+
 */
+
+// Here's a stash step.
+stage ("Write file then stash it") {
+    node {
+        // Make the output directory.
+        sh "mkdir -p stashedStuff"
+        // Let's write a bunch of junk to it. Same shell step as seen in 
+        // lots-of-logging-to-disk.
+        sh 'cat /dev/urandom | env LC_CTYPE=c tr -dc \'[:alpha:]\' | fold -w 5000 | head -n 1 > stashedStuff/5000characters'
+        stash name: "stashedFile1", includes: "stashedStuff/5000characters"
+    }
+}
--- testcases/explore/lots-of-logging-to-disk.groovy
+++ testcases/explore/lots-of-logging-to-disk.groovy
@@ -11,14 +11,6 @@ for (int i = 0; i < 5; i++) {
     stage ("Stage $i") {
         for (int j = 0; j < 5; j++) {
             node {
-                // Basic ls because there's no netstat on the lightweight system we're on
-                //   sh 'for i in `seq 1 100`; do echo "running loop $i"; netstat -a; done'
-                // This one is trying to restrict to alphanumerics. It also doesn't work.
-                //   sh 'for i in `seq 1 100`; do cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 3200 | head -n 1; done'
-                // Without the restriction to alphanumeric. This one works but generates gibberish.
-                // Might be fine.
-                //   sh 'for i in `seq 1 100` do cat /dev/urandom fold -w 500 | head -n 1; done'
-                // Sams thing
                 sh 'for i in `seq 1 100`; do echo "Stage $i Loop $j"; cat /dev/urandom | env LC_CTYPE=c tr -dc \'[:alpha:]\' | fold -w 320 | head -n 1; done'
             }
         }
