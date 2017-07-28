b63367b140830ec9b89210c9c784e08870e156b9
kshultzCB <kshultz@cloudbees.com>
2017-07-28 11:48:24 -0400

    Sorting job works!

--- testcases/explore/merge-sort.groovy
+++ testcases/explore/merge-sort.groovy
@@ -1,23 +1,22 @@
 /*
 merge-sort.groovy
 
-Oy.
+Merges the two integer arrays and then sorts them.
 */
 
 int[] firstThing = [1, 2, 3, -8, 44, 66, 3, 55, 996, 4, -33]
 int[] secondThing = [4, 5, -2231, 6, 3, 3, 7, 6, 99075, 4, 55, 126455, 555]
 
-
-System.out.println(firstThing);
-System.out.println(secondThing);
+// System.out.println(firstThing);
+// System.out.println(secondThing);
 
 merge (firstThing, secondThing);
 
 int[] merge(int[] first, int[] second) {
-    System.out.println("--> first is:  " + first);
-    System.out.println("--> second is: " + second);
+    // System.out.println("--> first is:  " + first);
+    // System.out.println("--> second is: " + second);
 	int[] output = new int[first.length + second.length];
-    System.out.println("--> output[] should be uninit'd: " + output + " with length of " + output.length);
+    // System.out.println("--> output[] should be uninit'd: " + output + " with length of " + output.length);
 	int idx = 0;
 	int i=0;
 	int j=0;
@@ -29,35 +28,35 @@ int[] merge(int[] first, int[] second) {
 		if (v1 > v2) {
 			output[idx++] = v1; 
 			i++;
-            System.out.println("1 --> in merge list output is now: " + output );
+            // System.out.println("1 --> in merge list output is now: " + output );
 		} else {
 			output[idx++] = v2;
 			j++;
-            System.out.println("1 --> in merge list output is now: " + output );
+            // System.out.println("1 --> in merge list output is now: " + output );
 		}
 	}
 
 	if ( i < first.length) {
 		for (int temp = i; i < first.length; i++) {
 			output[idx++] = first[i];
-            System.out.println("2 --> in merge list output is now: " + output );
+            // System.out.println("2 --> in merge list output is now: " + output );
 		}
 	} else if (j < second.length) {
 		for (int temp = j; j < second.length; j++) {
 			output[idx++] = second[j];
-            System.out.println("2 --> in merge list output is now: " + output );
+            // System.out.println("2 --> in merge list output is now: " + output );
 		}
 	}
     else {
         throw new IllegalStateException("Array length of i (" + i + ") and/or j (" + j + ") are all jacked up.");
     }
-	System.out.println("3 --> About to call output.sort()");
+	// System.out.println("3 --> About to call output.sort()");
 	output[].sort();
-	System.out.println("4 --> " + output);
+	// System.out.println("4 --> " + output);
 	Integer[] finallySorted = new Integer[output.length];
 	finallySorted = output;
 	finallySorted.sort();
-	System.out.println("--> Final answer is " + finallySorted);
+	// System.out.println("--> Final answer is " + finallySorted);
     return output;
 }
 
