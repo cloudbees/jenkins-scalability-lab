556056f307bd9998e630b4aaa4b6793beb3a9fcc
Sam Van Oort <samvanoort@gmail.com>
2017-08-01 18:15:24 -0400

    Note configuration for speaking to git repo to be added to groovy setup

--- jenkins/loadtestsetup.groovy
+++ jenkins/loadtestsetup.groovy
@@ -6,3 +6,14 @@ Jenkins.instance.setSecurityRealm(SecurityRealm.NO_AUTHENTICATION);
 
 // Solves issues with host URL for Swarm agent plugin
 JenkinsLocationConfiguration.get().setUrl("http://jenkins:8080/")
+
+/**
+Need to create new global credential:
+user: git
+SSH key from file - $JENKINS_HOME/id_rsa 
+
+For git repo, here's the config:
+ssh://git@gitserver:22/git-server/repos/repo-source.git
+use that credential
+
+*/
