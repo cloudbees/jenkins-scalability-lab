bcb04626352d33642018821800fbad6c41fe4a4f
Sam Van Oort <samvanoort@gmail.com>
2017-07-31 22:27:41 -0400

    Fix local startup script

--- local/build-local.sh
+++ local/build-local.sh
@@ -14,13 +14,16 @@ if [ $(docker network ls | grep scalability-bridge | wc -l) -eq 0]; then
     docker network create --attachable -d bridge scalability-bridge || true
 fi
 
+# Needed to pick up git configs and some other things
+CONFIG_DIR=$(cd .. && pwd)
+cp id_rsa.pub "${CONFIG_DIR}/gitserver/keys"
+cp id_rsa* "${CONFIG_DIR}/jenkins"
 docker build -t jenkins-scalability-master:2.0 ../jenkins
 
-# Needed to pick up git folder, note that the docker-compose fails due to network
-CONFIG_DIR=$(cd .. && pwd) # docker-compose up
+# When docker-compose networking is fixed to detect the bridge network this will work
+# CONFIG_DIR=$(cd .. && pwd) docker-compose up
 
 # Start git server, see keys from https://github.com/jkarlosb/git-server-docker
-cp id_rsa.pub gitserver/keys && cp id_rsa* jenkins
 LOCAL_DIR="$(pwd)"
 docker run --rm -d -p 2222:22 \
    --network scalability-bridge \
@@ -29,12 +32,6 @@ docker run --rm -d -p 2222:22 \
    -v "$LOCAL_DIR/gitserver/keys:/git-server/keys" \
    -v "$LOCAL_DIR/gitserver/repos:/git-server/repos" \
    jkarlos/git-server-docker
-
-# Autoconnect agents
-docker run -it --rm --network scalability-bridge \
-  --name agent -l role=agent \
-  -e "COMMAND_OPTIONS=-master http://jenkins:8080 -executors 4 -description swarm-slave -deleteExistingClients" \
-  vfarcic/jenkins-swarm-agent
 
 # Graphite server
 docker run --rm -d --network scalability-bridge \
@@ -47,9 +44,17 @@ docker run --rm -d --network scalability-bridge \
 ROOT_BLKDEV=/dev/$(docker run --rm -it jenkins-scalability-master:1.0 lsblk -d -o NAME | tail -n 1 | tr -d '\r' | tr -d '\n')
 
 # Run jenkins, specifying a named volume makes it persistent even after container dies
-docker run -it --rm  -h jenkins --name jenkins -l role=jenkins --network scalability-bridge \
+docker run --rm -d -h jenkins --name jenkins -l role=jenkins --network scalability-bridge \
   --device-write-iops $ROOT_BLKDEV:200 --device-write-bps $ROOT_BLKDEV:100mb --device-read-iops $ROOT_BLKDEV:200 --device-read-bps $ROOT_BLKDEV:100mb \
   -p 8080:8080 -p 9011:9011 \
   -v jenkins_home:/var/jenkins_home \
   jenkins-scalability-master:2.0
 #  -v $(pwd)/jenkins/jenkins_home:/var/jenkins_home \
+
+# Autoconnects & creates agents
+docker run --rm -d --network scalability-bridge \
+  --name agent -l role=agent \
+  -e "COMMAND_OPTIONS=-master http://jenkins:8080 -executors 4 -description swarm-slave -deleteExistingClients" \
+  vfarcic/jenkins-swarm-agent
+
+docker attach jenkins 
