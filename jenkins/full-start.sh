cd00c1afcb70e7ff15f995faa6486e2dd333fb11
Sam Van Oort <samvanoort@gmail.com>
2017-08-16 17:07:17 -0400

    Trying to fix those silly system properties

--- jenkins/full-start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Quick bolt-on to launch telegraf as well as Jenkins, with the telegraf process launching silently in the background
-telegraf 0<&- &>/dev/null &
-/usr/local/bin/jenkins.sh
