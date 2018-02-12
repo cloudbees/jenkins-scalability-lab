0876253a3f37896c652d5a723acd148df04f2dfe
Sam Van Oort <samvanoort@gmail.com>
2018-02-12 15:35:11 -0500

    Make tests shorter

--- runner/run-full-test.sh
+++ runner/run-full-test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Runs full test
-java -jar target/hydra-runner-1.0-SNAPSHOT-jar-with-dependencies.jar -t "legacy-pipeline,max-survivability" -f fulltest.txt -w 30000 -i localhost:8086 -j localhost:8080
+java -jar target/hydra-runner-1.0-SNAPSHOT-jar-with-dependencies.jar -t "legacy-pipeline,max-survivability" -f fulltest.txt -w 15000 -i localhost:8086 -j localhost:8080
