570b4d3d38b95f6f4cb6e7d2e5ec467008093ca8
Sam Van Oort <samvanoort@gmail.com>
2018-02-14 12:37:20 -0500

    Comment Hydra runner scripts and improve upon the scalability suites

--- aws/launch-local-grafana.sh
+++ aws/launch-local-grafana.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Launch grafana to speak to AWS
+# You'll need to customize the datasource to use the AWS Influx IP
 docker run --rm -d \
   -h grafana --name grafana \
   -p 81:3000 \
