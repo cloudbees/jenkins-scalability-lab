f2c114e35a8a6ec2509d8f1ab4ecb33f7753c744
Oleg Nenashev <o.v.nenashev@gmail.com>
2018-08-17 16:05:44 +0200

    [ARC-538] - Fix error handling in the script

--- aws/instance-preconfigure.sh
+++ aws/instance-preconfigure.sh
@@ -6,7 +6,7 @@ set -o pipefail
 sudo yum install -y git
 sudo yum install -y docker && sudo service docker start
 sudo chkconfig docker on
-sudo groupadd docker
+sudo groupadd docker || echo "Group already exists"
 sudo usermod -aG docker $USER
 
 # Add Maven installation?
