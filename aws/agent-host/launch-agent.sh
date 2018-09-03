e2b1d770e49f211dd6170ad2fe00a7156bc20149
Oleg Nenashev <o.v.nenashev@gmail.com>
2018-09-03 15:01:35 +0200

    Add a script for launching multiple agents in a burst

--- aws/agent-host/launch-agent.sh
+++ aws/agent-host/launch-agent.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+set -xe
 
 source "../settings.sh"
+AGENT_NUMBER=${1:-0}
 
 # Launch worker
 docker run --rm -d \
   --add-host jenkins:${IP_JENKINS_MASTER} --add-host gitserver:${IP_UTIL_CONTAINERS} \
-  --name agent -l role=agent \
+  --name agent_${AGENT_NUMBER} -l role=agent \
   -e "COMMAND_OPTIONS=-master http://jenkins:8080 -executors $(nproc) -description swarm-slave" \
   svanoort/jenkins-swarm-agent-mvn:1.0
